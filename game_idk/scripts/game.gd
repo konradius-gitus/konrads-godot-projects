db9644ca57579315fc7cc9a8bf45514981485475
Konrad Schulz <ks258@hdm-stuttgart.de>
2026-03-08 03:32:21 +0100

    added the game.gd singleton

---
+++ game_idk/scripts/game.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var current_level
+var current_state
+
+
+enum STATE{
+	MAIN_MENU,
+	
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func change_level():
+	pass
